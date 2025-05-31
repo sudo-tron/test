667c1e4bda44a030775668fb2bec3f4ab4de0903
sudo-tron <it@wayob.net>
2025-05-30 18:40:23 -0700

    Create arch.sh

---
+++ arch.sh
@@ -0,0 +1,4 @@
+sudo pacman -S plasma-desktop sddm xorg dolphin konsole ark flatpak plasma-pa kscreen powerdevil kde-gtk-config firefox discover xorg-xinit plasma-nm
+
+sudo systemctl enable sddm
+sudo systemctl start sddm
